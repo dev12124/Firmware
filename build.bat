8fc7d2881e9319e45c596a597862c78988f70061
dev12124 <joaoanandalima@gmail.com>
2026-08-16 03:15:19 -0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 set CC=clang
 set LD=ld.lld
 set CFLAGS=--target=x86_64-elf -ffreestanding -fno-builtin -Wall -Wextra -fno-exceptions -fno-rtti -mno-red-zone -nostdlib -I.
-set LDFLAGS=-m elf_x86_64 -T linker.ld
-%CC% %CFLAGS% -c IMAGE_BIOS_PDD.c -o IMAGE_BIOS_PDD.o && %LD% %LDFLAGS% IMAGE_BIOS_PDD.o -o IMAGE_BIOS_PDD.exe && llvm-objcopy -O binary IMAGE_BIOS_PDD.exe IMAGE_BIOS_PDD.bin
+set LDFLAGS=-m elf_x86_64 -T linker.ld --oformat binary
+%CC% %CFLAGS% -c IMAGE_BIOS_PDD.c -o IMAGE_BIOS_PDD.o && %LD% %LDFLAGS% IMAGE_BIOS_PDD.o -o IMAGE_BIOS_PDD.bin
     echo successfully generated!
